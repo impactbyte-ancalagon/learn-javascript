d1f8da8cae0da14aa885235cd0a52e98bad4c907
M Haidar Hanif <git@mhaidarhanif.com>
2018-12-17 15:29:35 +0700

    Create basic manipulation number program

---
+++ manipulation-number/index.js
@@ -0,0 +1,9 @@
+console.log("Manipulation Number")
+
+var number_a = 10
+var number_b = 2
+var result = number_a + number_b
+
+console.log("Number A:", number_a)
+console.log("Number B:", number_b)
+console.log(`Result = ${number_a} + ${number_b} = ${result}`)

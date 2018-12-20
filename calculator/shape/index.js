bb8c7d2135956d560d3f4defde6bb522392bd4d3
M Haidar Hanif <git@mhaidarhanif.com>
2018-12-20 10:47:01 +0700

    Create calculator shape example

---
+++ calculator/shape/index.js
@@ -0,0 +1,8 @@
+console.log("Calculator: Shape")
+
+const calculateSquareArea = side => {
+  return side ** 2
+}
+
+console.log("Square Area:", calculateSquareArea(4))
+console.log(`Square Area: ${calculateSquareArea(2)}`)

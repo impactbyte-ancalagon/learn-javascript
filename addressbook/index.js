73be226a3702d09aea7febe675b33c173fa0cab1
M Haidar Hanif <git@mhaidarhanif.com>
2018-12-20 11:46:17 +0700

    Create address book program with amazing data

--- addressbook/index.js
+++ addressbook/index.js
@@ -1,13 +1,13 @@
 console.log("Address Book")
 
-const contacts = [
+const DATA_CONTACTS = [
   {
     name: "Arsyad Sukma Ramadhan",
     nick: "Grandmaster",
     gender: "male",
     emails: ["blacklistdark1987@gmail.com", "181111072@mhs.stiki.ac.id"],
     phones: ["+6281284863756", "+6285785510045"],
-    adress: {
+    address: {
       country: "Indonesia",
       province: "Jawa Timur",
       city: "Malang",
@@ -31,7 +31,7 @@ const contacts = [
     gender: "male",
     emails: ["azerino25@gmail.com"],
     phones: ["+62895360159807", "+6281332466763"],
-    adress: {
+    address: {
       country: "Indonesia",
       province: "Jawa Timur",
       city: "Malang",
@@ -44,7 +44,7 @@ const contacts = [
     gender: "male",
     emails: ["ipulsaepulloh9@gmail.com"],
     phones: ["+62-858-8837-5440", "+62-859-7546-5858"],
-    adress: {
+    address: {
       country: "Indonesia",
       province: "Jawa Barat",
       city: "Karawang",
@@ -52,3 +52,46 @@ const contacts = [
     }
   }
 ]
+
+const addressBook = {
+  getData: data => {
+    return data
+  },
+  getNames: data => {
+    return data.map(item => {
+      return item.name
+    })
+  },
+  getFullContacts: data => {
+    return data.map(item => {
+      return `${item.name} (${item.phones[0]}) <${item.emails[0]}>`
+    })
+  },
+  getFullAddresses: data => {
+    return data.map(item => {
+      const { city, province, country } = item.address
+      return `${item.nick} lives in ${city}, ${province}, ${country}`
+    })
+  },
+  displayConsole: data => {
+    data.forEach(item => {
+      console.log(item)
+    })
+  },
+  displayHTML: data => {
+    data.forEach(item => {
+      document.write(`<li>${item}</li>`)
+    })
+  }
+}
+
+console.log(addressBook.getData(DATA_CONTACTS))
+
+const fullContacts = addressBook.getFullContacts(DATA_CONTACTS)
+const fullAddresses = addressBook.getFullAddresses(DATA_CONTACTS)
+
+addressBook.displayConsole(fullContacts)
+addressBook.displayConsole(fullAddresses)
+
+addressBook.displayHTML(fullContacts)
+addressBook.displayHTML(fullAddresses)

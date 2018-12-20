3b1f2a5d4afb50ac23de0a6a6e398a2e147b8027
M Haidar Hanif <git@mhaidarhanif.com>
2018-12-20 11:32:44 +0700

    Create dummy data for address book

---
+++ addressbook/index.js
@@ -0,0 +1,54 @@
+console.log("Address Book")
+
+const contacts = [
+  {
+    name: "Arsyad Sukma Ramadhan",
+    nick: "Grandmaster",
+    gender: "male",
+    emails: ["blacklistdark1987@gmail.com", "181111072@mhs.stiki.ac.id"],
+    phones: ["+6281284863756", "+6285785510045"],
+    adress: {
+      country: "Indonesia",
+      province: "Jawa Timur",
+      city: "Malang",
+      zip: "65154"
+    },
+    username: "arsyadsr09",
+    socials: [
+      {
+        name: "GitHub",
+        url: "https://github.com/arsyadsr09"
+      },
+      {
+        name: "instagram",
+        url: "https://instagram.com/arsyadsr09"
+      }
+    ]
+  },
+  {
+    name: "Azerino Yogananta Gatot Subroto",
+    nick: "Gatot / Yoga",
+    gender: "male",
+    emails: ["azerino25@gmail.com"],
+    phones: ["+62895360159807", "+6281332466763"],
+    adress: {
+      country: "Indonesia",
+      province: "Jawa Timur",
+      city: "Malang",
+      zip: "65123"
+    }
+  },
+  {
+    name: "Ipul Saepulloh",
+    nick: "Ipul",
+    gender: "male",
+    emails: ["ipulsaepulloh9@gmail.com"],
+    phones: ["+62-858-8837-5440", "+62-859-7546-5858"],
+    adress: {
+      country: "Indonesia",
+      province: "Jawa Barat",
+      city: "Karawang",
+      zip: "41374"
+    }
+  }
+]

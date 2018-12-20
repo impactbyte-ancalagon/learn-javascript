6bb077ef396f18fe3f44c61a86af5f72e9e89d76
M Haidar Hanif <git@mhaidarhanif.com>
2018-12-20 10:43:09 +0700

    Create example with functions in object

--- calculator/math/index.js
+++ calculator/math/index.js
@@ -1,15 +1,34 @@
 console.log("Calculator: Math")
 
+// -----------------------------------------------------------------------------
+
 const add = (number1, number2) => {
   if (typeof number1 === "number" && typeof number2 === "number")
     return number1 + number2
   else return "One or more of the parameter(s) is/are not a number"
 }
+
 const substract = (number1, number2) => number1 - number2
 const multiply = (number1, number2) => number1 * number2
 const divide = (number1, number2) => number1 / number2
-const module = (number1, number2) => number1 % number2
+const modulo = (number1, number2) => number1 % number2
 const power = (number1, number2) => number1 ** number2
+
+// -----------------------------------------------------------------------------
+
+const math = {
+  add: (x, y) => x + y,
+  substract: (x, y) => x - y,
+  divide: (x, y) => x / y,
+  modulo: (x, y) => x % y,
+  power: (x, y) => x ** y
+}
+
+const something = {
+  add: (x, y) => x + y + "!"
+}
+
+// -----------------------------------------------------------------------------
 
 const result_add = add(10, 20)
 const result_power = power(2, 3)
